db4d05bffa4f19996bb35b19da8a59c4e4c4c54e
simekj21 <43533811+simekj21@users.noreply.github.com>
2024-04-12 08:58:54 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h3 class="text-2xl font-bold text-center p-4">New change 4</h3>
+  <h3 class="text-2xl font-bold text-center p-4">New change on main branch</h3>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
     <a 
       v-for="url in urls" 
